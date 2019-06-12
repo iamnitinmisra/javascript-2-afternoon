f4cd7c6cd10d5b01c26497aebe4a0f31d1f023ec
Nitin Misra <nitin.misra84@gmail.com>
2019-06-12 14:30:59 -0700

    practice problems through #4 completed

--- practice.js
+++ practice.js
@@ -16,6 +16,14 @@
 
 //Code here
 
+let me = {
+  name: "Nitin Misra",
+  age: 35
+}
+
+//window.alert(me.name)
+
+
 ////////// PROBLEM 2 //////////
 
 /*
@@ -25,12 +33,24 @@
 
 //Code here
 
+let favoriteThings = {
+  band: "Muse",
+  food: "Sushi",
+  person: "Marissa",
+  book: "A Song of Ice and Fire",
+  movie: "The Matrix",
+  holiday: "Thanksgiving"
+}
+
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
 //Code here
+favoriteThings.car = "Ferrari F50";
+favoriteThings.brand = "Nike";
+
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
@@ -38,6 +58,8 @@
 */
 
 //Code here
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 
 ////////// PROBLEM 3 //////////
 
@@ -50,13 +72,18 @@
 */
 
 //Code here
+let backPack = {};
+let item = "firstPocket";
+backPack[item] = "chapstick";
+backPack.color = "black";
+
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
-
+window.alert(backPack);
 /*
 You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -64,6 +91,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
+console.log(backPack)
 
 ////////// PROBLEM 4 //////////
 
@@ -85,6 +113,9 @@ var user2 = {
 */
 
 //Code Here
+user2.name = "Bryan G. Smith";
+user2.email = "bryan.smith@devmounta.in"
+
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
@@ -96,6 +127,7 @@ var user2 = {
 */
 
 //Code Here
+let methodCollection = {};
 
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
